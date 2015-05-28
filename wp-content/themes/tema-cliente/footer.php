141ce3a830e08a55055c9311645fc39445986132
Raul Vinicius <raul.portifolio@gmail.com>
2015-05-27 22:57:03 -0300

    limpeza e atualizacao dos plugins

--- wp-content/themes/tema-cliente/footer.php
+++ wp-content/themes/tema-cliente/footer.php
@@ -7,15 +7,17 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src= "<?php bloginfo("template_url") ?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-        <script src= "<?php bloginfo('template_url') ?>/js/vendor/bootstrap.min.js"></script>
+        <script src= "<?php bloginfo('template_url') ?>/js/bootstrap.min.js"></script>
+        <script src= "<?php bloginfo('template_url') ?>/js/vendor/modernizr-2.8.3.min.js"></script>
 
         <script src= "<?php bloginfo('template_url') ?>/js/main.js"></script>
-        <script src="<?php bloginfo('template_url') ?>/js/jquery.mask.js"></script>
+        <script src="<?php bloginfo('template_url') ?>/js/jquery.mask.min.js"></script>
         <script src="<?php bloginfo('template_url') ?>/js/jquery.form.js"></script>
         <script src="<?php bloginfo('template_url') ?>/js/jquery.validate.min.js"></script>
-        <script src="<?php bloginfo('template_url') ?>/js/jquery.cookie.js"></script>
+        <!-- <script src="<?php bloginfo('template_url') ?>/js/jquery.cookie.js"></script> -->
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <!-- 
         <script>
             (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
             function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -24,6 +26,7 @@
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script>
+         -->
 
     </body>
 
